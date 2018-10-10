f517315dccf7bddc3ef95627742fba9a801c9c92
NesamaniRajamanickam <43475324+NesamaniRajamanickam@users.noreply.github.com>
2018-10-10 15:20:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -18,7 +18,9 @@ import io
 
 
 
-app = dash.Dash()
+#app = dash.Dash()
+app = dash.Dash(__name__)
+server = app.server
 
 app.scripts.config.serve_locally = True
 app.config['suppress_callback_exceptions'] = True
